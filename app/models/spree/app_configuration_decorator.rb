ade0cd10c44f2f9a7833db810520511328ee0a32
Alessio Rocco <alessio.rocco.lt@gmail.com>
2014-09-17 15:05:40 +0200

    adds preferences

---
+++ app/models/spree/app_configuration_decorator.rb
@@ -0,0 +1,23 @@
+Spree::AppConfiguration.class_eval do
+  preference :order_start_date_constraint, :string
+  preference :order_end_date_constraint, :string
+  preference :order_line_items_constraint, :integer
+
+  def order_allowed_time_range?
+    order_constraint_range.cover?(Time.now.to_f)
+  end
+
+  def order_start_date_constraint
+    get(:order_start_date_constraint).to_time
+  end
+
+  def order_end_date_constraint
+    get(:order_end_date_constraint).to_time
+  end
+
+  def order_constraint_range
+    start_date = order_start_date_constraint || Time.now
+    end_date = order_end_date_constraint || Float::INFINITY
+    start_date.to_f..end_date.to_f
+  end
+end

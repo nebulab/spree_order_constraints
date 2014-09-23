4396d38956cb2abe1b2bcd3579bcddd940ac2d3c
Matteo Latini <mtylty@gmail.com>
2014-09-23 14:31:30 +0200

    removes some lefover names

--- lib/generators/spree_order_constrains/install/install_generator.rb
+++ lib/generators/spree_order_constrains/install/install_generator.rb
@@ -1,4 +1,4 @@
-module SpreeOrderConstrains
+module SpreeOrderConstraints
   module Generators
     class InstallGenerator < Rails::Generators::Base
 
--- lib/spree_order_constraints/engine.rb
+++ lib/spree_order_constraints/engine.rb
@@ -1,4 +1,4 @@
-module SpreeOrderConstrains
+module SpreeOrderConstraints
   class Engine < Rails::Engine
     require 'spree/core'
     isolate_namespace Spree
